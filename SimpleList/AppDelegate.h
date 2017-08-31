4a3619173627f3d23f2e377e64601f5219d2f5ef
Paul Stringer <paulstringer@mac.com>
2017-08-31 12:07:41 +0100

    Initial Commit

---
+++ SimpleList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleList
+//
+//  Created by Paul Stringer on 31/08/2017.
+//  Copyright © 2017 kiskynetmedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleList/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SimpleList
+//
+//  Created by Paul Stringer on 31/08/2017.
+//  Copyright © 2017 kiskynetmedia. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+
+@end
